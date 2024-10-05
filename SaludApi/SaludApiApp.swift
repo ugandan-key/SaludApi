692b2932faaf75b53c4001599d2a2a92925863d1
Geovani Adrián Monroy García <126182858+ugandan-key@users.noreply.github.com>
2024-10-04 21:16:56 -0600

    Initial Commit

---
+++ SaludApi/SaludApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaludApiApp.swift
+//  SaludApi
+//
+//  Created by Geovany Monroy Garcia on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SaludApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SaludApiTests/SaludApiTests.swift
@@ -0,0 +1,17 @@
+//
+//  SaludApiTests.swift
+//  SaludApiTests
+//
+//  Created by Geovany Monroy Garcia on 4/10/24.
+//
+
+import Testing
+@testable import SaludApi
+
+struct SaludApiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
